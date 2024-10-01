f8c3667738d96bda7e4476c8d3ed567d779881ee
Piyush Dubey <piyush.dubey@hp.com>
2024-10-01 13:35:40 +0530

    changing the account number and repo name

--- config/config.ts
+++ config/config.ts
@@ -7,14 +7,14 @@ import { DevUsEast } from "./dev/devus-east-1";
 export const AppConfig={
     hpdemoDev:{
         env: {
-            account: "034362056347",
+            account: "015021686405",
             region: "us-east-1"
         },
         pipelineName: "hp-dev-demo-multiregion-github",
         pipelineStackName: "hp-dev-demo-multiregion-github",
-        repoName: "hp-demo-ci-cd-multiRegion",
+        repoName: "hp-demo-ci-cd-multiregion-v1",
         repoTriggerBranch: "dev",
-        githubOrg:"piyush1008",
+        githubOrg:"CSS-IT-Integration",
         stages: [DevEuCentral, DevUsEast ],
     }
 }
--- config/dev/deveu-central-1.ts
+++ config/dev/deveu-central-1.ts
@@ -4,7 +4,7 @@ import { ApplicationStageProps } from "../../model/ApplicationStageProps";
 
 export const DevEuCentral:ApplicationStageProps={
     env: {
-        account: "034362056347",
+        account: "015021686405",
         region: "eu-central-1",
     },
     client: "hpdemo",
